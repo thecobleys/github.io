47232f8d3812122e8d4d0118485f3ad8dd22edff
zmek <zella.king@gmail.com>
2022-12-04 22:24:16 +0000

    tried js fittext function

--- index.js
+++ index.js
@@ -31,3 +31,35 @@ $("section").each(function (index) {
     console.log(index + ": " + $(this).class());
   });
 });
+
+function fitText(outputSelector){
+  // max font size in pixels
+  const maxFontSize = 50;
+  // get the DOM output element by its selector
+  let outputDiv = document.getElementById(outputSelector);
+  // get element's width
+  let width = outputDiv.clientWidth;
+  // get content's width
+  let contentWidth = outputDiv.scrollWidth;
+  // get fontSize
+  let fontSize = parseInt(window.getComputedStyle(outputDiv, null).getPropertyValue('font-size'),10);
+  // if content's width is bigger then elements width - overflow
+  if (contentWidth > width){
+      fontSize = Math.ceil(fontSize * width/contentWidth,10);
+      fontSize =  fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize - 1;
+      outputDiv.style.fontSize = fontSize+'px';   
+  }else{
+      // content is smaller then width... let's resize in 1 px until it fits 
+      while (contentWidth === width && fontSize < maxFontSize){
+          fontSize = Math.ceil(fontSize) + 1;
+          fontSize = fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize;
+          outputDiv.style.fontSize = fontSize+'px';   
+          // update widths
+          width = outputDiv.clientWidth;
+          contentWidth = outputDiv.scrollWidth;
+          if (contentWidth > width){
+              outputDiv.style.fontSize = fontSize-1+'px'; 
+          }
+      }
+  }
+}
